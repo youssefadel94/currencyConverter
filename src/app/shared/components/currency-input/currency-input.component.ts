7a2deaa8ac3b80c6a5779e834ca3c0c17e54231d
Youssef adel <darkmetalxdragon@hotmail.com>
2022-07-17 00:25:22 +0200

    init style

--- src/app/shared/components/currency-input/currency-input.component.ts
+++ src/app/shared/components/currency-input/currency-input.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ControlValueAccessor, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-currency-input',
@@ -9,9 +10,18 @@ export class CurrencyInputComponent implements OnInit {
   @Output() onChange = new EventEmitter();
   @Input() loading = false;
   delayTimer = setTimeout(function () {}, 1000);
+
+  // form group with control value amount
+  form = new FormBuilder().group({
+    amount: [1, [Validators.required, Validators.min(1)]],
+  });
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.form.get('amount')?.valueChanges.subscribe((value) => {
+      this.inputChange({ target: { value } });
+    });
+  }
   inputChange($event: any) {
     clearTimeout(this.delayTimer);
     let that = this;
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 import { CurrencyInputComponent } from './components/currency-input/currency-input.component';
 import { CurrenciesComponent } from './components/currencies/currencies.component';
 import { CurrencyCardComponent } from './components/currency-card/currency-card.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [CurrencyInputComponent, CurrenciesComponent, CurrencyCardComponent],
+  declarations: [
+    CurrencyInputComponent,
+    CurrenciesComponent,
+    CurrencyCardComponent,
+  ],
   exports: [CurrencyInputComponent, CurrenciesComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
 })
 export class SharedModule {}
